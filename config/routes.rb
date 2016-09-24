12883390f71e32f4969aa9e8b2a24365025bf71a
Ryan Morrison <ryanam26@yahoo.com>
2016-09-24 16:59:27 -0400

    added landing page, signin and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  
   devise_for :users, :controllers => {:registrations => "user/registrations"}
+
+	root 'welcome#index'
+  
   resources :user_stocks, except: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root 'welcome#index'
+	# root :to => "landingpage#index"  added yesterday
+
+
+	# root 'welcome#index'
+	# root 'landingpage#index'  added yesterday
 
 	get 'my_portfolio', to: 'users#my_portfolio'
 	get 'search_stocks', to: 'stocks#search'
