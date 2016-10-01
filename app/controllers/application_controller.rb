41bb3c4964745a45e429218cf263f945c53a271a
Ryan Morrison <ryanam26@yahoo.com>
2016-10-01 17:05:03 -0400

    fixed the footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # before_action :authenticate_user!
+
+
+
+  protected
+
+ 	def after_sign_in_path_for(resource)
+  	my_portfolio_path
+	end
+
+	def after_sign_up_path_for(resource)
+  	my_portfolio_path
+	end
+
+
 end
